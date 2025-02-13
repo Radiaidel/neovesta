4a499124f761450905731ea87a321c736a9d7ae1
radia idelkadi <idelkadiradia@gmail.com>
2025-02-13 10:48:34 +0100

    Initial commit - Backend & Frontend setup

---
+++ backend/src/main/java/com/neovesta/backend/NeovestaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.neovesta.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeovestaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeovestaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/neovesta/backend/NeovestaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neovesta.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeovestaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
