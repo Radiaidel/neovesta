0c47e2d46c790d35b89a2f404784eada4ab2bc03
radia idelkadi <idelkadiradia@gmail.com>
2025-03-13 12:23:42 +0000

    Revamp landing page & footer design for a modern look

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -10,7 +10,7 @@ import { ToastService } from "../../services/toast.service"
 @Component({
   selector: "app-login",
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule , RouterLink],
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"],
 })
