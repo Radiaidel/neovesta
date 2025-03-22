207436e88e99a63572ab326df25132df0f8fece4
radia idelkadi <idelkadiradia@gmail.com>
2025-03-22 11:14:36 +0000

    Fix environment variables loading + add .env to .gitignore file

--- backend/src/main/java/com/neovesta/backend/NeovestaBackendApplication.java
+++ backend/src/main/java/com/neovesta/backend/NeovestaBackendApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NeovestaBackendApplication {
-
-	public static void main(String[] args) {
-
-		SpringApplication.run(NeovestaBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(NeovestaBackendApplication.class, args);
+    }
 }
--- backend/src/main/java/com/neovesta/backend/config/EnvConfig.java
+++
@@ -1,9 +0,0 @@
-package com.neovesta.backend.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("file:./.env")
-public class EnvConfig {
-}
